f98a2473940f7fdd13f85cd6213dad70799a1b95
Paul Miller <paul@paul.lol>
2025-09-25 11:18:25 -0500

    fix path deps

--- demos/map-coordinates/vite.config.ts
+++ demos/map-coordinates/vite.config.ts
@@ -3,21 +3,12 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const rootDir = fileURLToPath(new URL(".", import.meta.url));
-const moqDir = path.resolve(rootDir, "../../../moq");
 
 export default defineConfig({
   server: {
     port: 5175,
     fs: {
-      allow: [rootDir, moqDir],
+      allow: [rootDir],
     },
-  },
-  resolve: {
-    alias: {
-      "@kixelated/moq": path.resolve(rootDir, "../../../moq/js/moq/src/index.ts"),
-      "@kixelated/moq/": path.resolve(rootDir, "../../../moq/js/moq/src") + "/",
-      "@kixelated/signals": path.resolve(rootDir, "../../../moq/js/signals/src/index.ts"),
-      "@kixelated/signals/": path.resolve(rootDir, "../../../moq/js/signals/src") + "/",
-    },
-  },
+  } 
 });
